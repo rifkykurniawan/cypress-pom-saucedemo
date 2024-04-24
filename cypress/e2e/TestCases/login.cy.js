b30acb839fa5f37870806f604cccbed04fa44334
rifkykurniawan <toettoet.tt@gmail.com>
2024-04-24 17:33:57 +0700

    add before on login.cy.js

--- cypress/e2e/TestCases/login.cy.js
+++ cypress/e2e/TestCases/login.cy.js
@@ -2,8 +2,11 @@ import { loginPageElements } from "../../../PageObject/PageActions/LoginPage";
 
 const Login_Elements = new loginPageElements
 describe('POM test suite', () => {
-    it('Login Page', function(){
+    before(() => {
         cy.visit('https://www.saucedemo.com/v1/')
+    });
+    
+    it('Login Page', function(){
         Login_Elements.username('standard_user')
         Login_Elements.password('secret_sauce')
         Login_Elements.loginButton()

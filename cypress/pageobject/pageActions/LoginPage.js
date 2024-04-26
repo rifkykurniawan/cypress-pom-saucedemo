433277a314776f20ae722b03264755ab1fe3471c
rifkykurniawan <toettoet.tt@gmail.com>
2024-04-26 09:11:41 +0700

    update folder name

--- cypress/pageobject/pageActions/LoginPage.js
+++ cypress/pageobject/pageActions/LoginPage.js
@@ -1,26 +1,24 @@
-const loginElementLocators = require('../../pageobject/pageElements/LoginPageElements.json')
+const locator = require('../../pageobject/pageElements/Locator.json')
 export class loginPageElements {
     username(username_data){
-        cy.get(loginElementLocators.LoginPageLocator.username_text).type(username_data)
+        cy.get(locator.LoginPage.username_text).type(username_data)
         return
     }
     password(password_data){
-        cy.get(loginElementLocators.LoginPageLocator.password_text).type(password_data)
+        cy.get(locator.LoginPage.password_text).type(password_data)
         return
     }
     loginButton(){
-        cy.get(loginElementLocators.LoginPageLocator.login_button).click()
-        return
+        cy.get(locator.LoginPage.login_button).click() 
     }
     verifyLogin(){
-        cy.get(loginElementLocators.HomePageLocator.homepage_title).should('be.visible')
-        return
+        cy.get(locator.HomePage.homepage_title).should('be.visible')
     }
     verifyFailedLogin(){
-        cy.get(loginElementLocators.LoginPageLocator.error_message).should('be.visible')
+        cy.get(locator.LoginPage.error_message).should('be.visible')
     }
     verifyLoginPage(){
-        cy.get(loginElementLocators.LoginPageLocator.username_text).should('be.visible')
-        cy.get(loginElementLocators.LoginPageLocator.password_text).should('be.visible')
+        cy.get(locator.LoginPage.username_text).should('be.visible')
+        cy.get(locator.LoginPage.password_text).should('be.visible')
     }
 }
